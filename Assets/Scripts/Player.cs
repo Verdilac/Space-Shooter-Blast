8156a2dd95b29c250962eea4a8c8a333914e0cd0
lathindu <64456879+Verdilac@users.noreply.github.com>
2021-04-26 16:35:16 +0530

    First Commit
    First Commit After tripleshot

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    //[SerializeField] GameObject player;
+    [SerializeField] private float _speed = 3.5f;
+    [SerializeField] public GameObject _laserPrefab;
+    [SerializeField] private GameObject _tripleShotPrefab;
+    [SerializeField] private float _fireRate = 0.15f;
+    [SerializeField] private int _lives = 3;
+    private SpawnManager _spawnManager;
+    private bool _tripleShotActive = false ;
+
+    
+    private float _canFire = -1f;
+
+    void Start()
+    {
+        //Zeroing out the Player Object
+        transform.position = new Vector3(0, 0, 0);
+        _spawnManager = GameObject.Find("Spawn_Manager").GetComponent<SpawnManager>();
+        
+        if (_spawnManager == null)
+        {
+            Debug.LogError("The Spawn Manager is NULL");
+        }
+        
+
+    }
+
+  
+    void Update()
+    {
+        CalculateMovement();
+
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > _canFire)
+        {
+            FireLaser();
+        }
+
+
+    }
+
+
+
+    void CalculateMovement()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+
+        Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);
+
+
+        //time.delta time is the time you have between 2 frames typically very close to 1 second.
+        transform.Translate(direction * _speed * Time.deltaTime);
+
+        if (transform.position.x > 11.35)
+        {
+            transform.position = new Vector3(-11.35f, transform.position.y, transform.position.z);
+        }
+        else if (transform.position.x < -11.35)
+        {
+            transform.position = new Vector3(11.35f, transform.position.y, transform.position.z);
+        }
+
+        transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, -4.98f, 0), 0);
+    }
+
+    void FireLaser()
+    {
+
+        //the reason we re incrementing this is we need the statement fail inbetween firing and the next shot.so while (*ex)time might be 1 can fire would be 1.5 and untill time hit 1.6 you cant fire
+        
+        if (!_tripleShotActive)
+        {
+            _canFire = Time.time + _fireRate;
+            Instantiate(_laserPrefab, transform.position + new Vector3(0, 1.2f, 0), Quaternion.identity);
+        }
+        else
+        {
+            _canFire = Time.time + _fireRate;
+            Instantiate(_tripleShotPrefab, transform.position + new Vector3(0, 1.2f, 0), Quaternion.identity);
+            _tripleShotActive = false;
+        }
+        
+       
+    }
+
+    public void Damage()
+    {
+        _lives --;
+
+        if (_lives < 1)
+        {
+            _spawnManager.OnPlayerDeath();
+            Destroy(this.gameObject);
+        }
+
+    }
+
+    public void ActivateTripleShot()
+    {
+        _tripleShotActive = true;
+    }
+
+}
---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    [SerializeField] public GameObject _enemyPrefab;
+    [SerializeField ] private GameObject _enemyContainer;
+    private bool _stopSpawning = false;
+    void Start()
+    {
+         StartCoroutine(SpawnRoutine());
+    }
+
+  
+    void  Update()
+    {
+       
+    }  
+    IEnumerator SpawnRoutine()
+        {
+            while (_stopSpawning == false)
+            {
+                float randomX = Random.Range(-8.70f, 8.70f);
+                Vector3 posToSpawn = new Vector3(randomX, 6.44f, 0);
+                GameObject newEnemy =  Instantiate(_enemyPrefab, posToSpawn,Quaternion.identity);
+                newEnemy.transform.parent = _enemyContainer.transform;
+                yield return new WaitForSeconds(5.0f);
+            }
+        }
+
+    public void OnPlayerDeath()
+    {
+        _stopSpawning = true;
+        Destroy(_enemyContainer);
+    }
+}
